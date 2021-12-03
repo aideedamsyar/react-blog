83e1c4ac4724902513b58109b19adff19ad462b8
Tengku Aideed Amsyar <aideedamsyar@gmail.com>
2021-12-04 03:59:09 +0800

    first- commit

---
+++ src/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> { // to fetch json
+        const abortCont = new AbortController();
+        fetch(url, {signal: abortCont.signal})
+            .then(res => {
+                console.log(res);
+                if(!res.ok) {
+                    throw Error('Could not fetch the data for that resource bro :(');
+                }
+                return res.json()
+            })
+            .then((data) => { //when data is fully loaded
+                console.log(data);
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsLoading(false);
+                    setError(err.message);
+                }
+
+            })
+
+            return () => abortCont.abort();
+    }, [url]);
+
+    return { data, isLoading, error };
+}
+
+export default useFetch;
